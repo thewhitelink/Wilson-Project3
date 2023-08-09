25200be5d888aebb0a7b4813e2e10f42db11417a
Daniel Wilson <dwilson05lh@gmail.com>
2023-08-09 11:08:03 -0400

    updated admin name

--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -17,7 +17,7 @@ resource "azurerm_linux_virtual_machine" "VM" {
   resource_group_name   = "${var.resource_group}"
   size                  = "Standard_B2s"
   disable_password_authentication = "false"
-  admin_username        = "admin"
+  admin_username        = "daniel"
   admin_password        = "Password123!"
   network_interface_ids = [
     azurerm_network_interface.int.id
