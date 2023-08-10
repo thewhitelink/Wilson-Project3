ed9c33710994a9ccd560c6079f36f6c797850f5e
Daniel Wilson <dwilson05lh@gmail.com>
2023-08-10 16:50:29 -0400

    terraform app changes

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,23 +2,22 @@ resource "azurerm_service_plan" "test" {
   name                = "ASP-${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Windows"
+  os_type             = "Linux"
   sku_name            = "F1"
 }
 
-resource "azurerm_windows_web_app" "test" {
+resource "azurerm_linux_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
   service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = 1
-    "SCM_DO_BUILD_DURING_DEPLOYMENT" = true
+    "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
   site_config {
     always_on = false
-    }
+  }
   }
   #lifecycle {
   #  ignore_changes = [
