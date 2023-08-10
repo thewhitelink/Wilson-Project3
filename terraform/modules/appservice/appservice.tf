5cfae5112ed5c427f706c7ddaf8a222f1911df9c
Daniel Wilson <dwilson05lh@gmail.com>
2023-08-10 16:09:23 -0400

    web package change

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -13,7 +13,7 @@ resource "azurerm_linux_web_app" "test" {
   service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = 1
+    "WEBSITE_RUN_FROM_PACKAGE" = 0
   }
   site_config {
     always_on = false
