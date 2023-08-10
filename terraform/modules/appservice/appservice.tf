101bbfaa046fec29ffd86b39b1ad6987c0980240
Daniel Wilson <dwilson05lh@gmail.com>
2023-08-10 13:52:18 -0400

    changed to windows app

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -2,7 +2,7 @@ resource "azurerm_service_plan" "test" {
   name                = "ASP-${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
-  os_type             = "Linux"
+  os_type             = "Windows"
   sku_name            = "F1"
 }
 
@@ -13,7 +13,7 @@ resource "azurerm_linux_web_app" "test" {
   service_plan_id     = azurerm_service_plan.test.id
 
   app_settings = {
-    "WEBSITE_RUN_FROM_PACKAGE" = 0
+    "WEBSITE_RUN_FROM_PACKAGE" = 1
   }
   site_config {
     always_on = false
