de0ee64d87ba8915cec773c5cf537680c3f4d88c
Daniel Wilson <dwilson05lh@gmail.com>
2023-08-10 18:01:59 -0400

    changed webapp

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -6,7 +6,7 @@ resource "azurerm_service_plan" "test" {
   sku_name            = "F1"
 }
 
-resource "azurerm_windows_web_app" "test" {
+resource "azurerm_linux_web_app" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
@@ -19,10 +19,10 @@ resource "azurerm_windows_web_app" "test" {
   }
   site_config {
     always_on = false
-    application_stack {
-        current_stack = "dotnet"
-        dotnet_version = "v6.0"
-    }
+    #application_stack {
+    #    current_stack = "dotnet"
+    #    dotnet_version = "v6.0"
+    #}
   }
   }
   #lifecycle {
