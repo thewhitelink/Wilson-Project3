b92a4cb58c9bb604f117ad8cb800072586717bb6
Daniel <dwilson05lh@gmail.com>
2023-08-13 18:37:00 -0400

    fix

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -19,14 +19,14 @@ from selenium import webdriver
 
 #options.capabilities("google-chrome")
 #options.binary_location = ("/usr/bin/google-chrome")
-chrome_options = Options()
+chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = "/usr/bin/google-chrome"
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-driver = webdriver.Chrome("/usr/bin/chromedriver", options=chrome_options)
+driver = webdriver.Chrome(executable_path="/usr/bin/chromedriver", options=chrome_options)
 
 # # Print date and time
 # current_datetime = datetime.datetime.now()
