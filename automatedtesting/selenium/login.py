7ee0b4f915a930e6dffc8e0ddc8e4305b5dbe397
Daniel <dwilson05lh@gmail.com>
2023-08-13 17:14:40 -0400

    test

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -16,7 +16,7 @@ from selenium.webdriver.chrome.options import Options
 # driver = webdriver.Chrome('/usr/bin/chromedriver')#, options=None)
 
 options = webdriver.ChromeOptions()
-options.capabilities("google-chrome")
+#options.capabilities("google-chrome")
 options.binary_location = ("/usr/bin/google-chrome")
 options.add_argument("--headless")
 options.add_argument("--no-sandbox")
