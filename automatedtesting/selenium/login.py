5b250c3f41d1202df7f137e17d01949d26e10ed5
Daniel <dwilson05lh@gmail.com>
2023-08-13 18:02:43 -0400

    rem service

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -2,7 +2,6 @@
 import time
 import datetime
 from selenium import webdriver
-from selenium import Service
 #from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
