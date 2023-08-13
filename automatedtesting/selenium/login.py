a49821a981ccd70934a27e5b178d403d4df7c295
Daniel <dwilson05lh@gmail.com>
2023-08-13 18:48:12 -0400

    removed options

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -25,7 +25,7 @@ chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
-driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
 # # Print date and time
 # current_datetime = datetime.datetime.now()
