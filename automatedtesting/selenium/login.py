6e2184065817b3bcb2b362e191a5df35cf59bf77
Daniel <dwilson05lh@gmail.com>
2023-08-13 19:19:22 -0400

    fix again

--- automatedtesting/selenium/login.py
+++ automatedtesting/selenium/login.py
@@ -10,6 +10,7 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument("--no-sandbox")
 chrome_options.add_argument('--disable-dev-shm-usage')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+chrome_options.binary_location = "/usr/bin/google-chrome"
 
 # Set the PATH environment variable to include the location of ChromeDriver
 import os
